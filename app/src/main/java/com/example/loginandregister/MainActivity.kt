63f35771f72eeccc888d38c8b4e885224732ac22
df <>
2021-07-18 23:14:05 +0100

    A login and registeration app with firebase

---
+++ app/src/main/java/com/example/loginandregister/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.loginandregister
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnRegisterLogin.setOnClickListener{
+            startActivity(Intent(this,RegisterActivity::class.java))
+            overridePendingTransition(R.anim.slide_from_right,R.anim.slide_to_left)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/loginandregister/RegisterActivity.kt
@@ -0,0 +1,22 @@
+package com.example.loginandregister
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        btnLoginRegister.setOnClickListener{
+            onBackPressed()
+
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        overridePendingTransition(R.anim.slide_from_left,R.anim.slide_to_right)
+    }
+}
